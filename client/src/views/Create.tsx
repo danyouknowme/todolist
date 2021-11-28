6936a5398a557f83f02471ab9f47baa6bc801c7b
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-29 01:45:25 +0700

    add create page

--- client/src/views/Create.tsx
+++ client/src/views/Create.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { AddCircle, Info } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import './show.scss';
+import './create.scss';
 
 const Create = () => {
+    let history = useHistory();
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+
+    const createTask = () => {
+        axios.post('http://localhost:3000/api/lists', {
+            title,
+            description
+        })
+        .then((response) => {
+            console.log(response)
+            history.push('/');
+        })
+        .catch((error) => console.log(error));
+    }
+
     return (
-        <div>
-            Create page
+        <div className="show">
+            <div className="wrapper">
+                <h1>Show task</h1>
+                <div>
+                    <div className="task">
+                        <AddCircle />
+                        <span>Task</span>
+                    </div>
+                    <div className="content">
+                        <input type="text" placeholder="Enter task..." onChange={(e) => setTitle(e.target.value)}/>
+                    </div>
+                </div>
+                <div>
+                    <div className="task">
+                        <Info />
+                        <span>Detail</span>
+                    </div>
+                    <div className="content">
+                        <input type="text" placeholder="Enter details..." onChange={(e) => setDescription(e.target.value)}/>
+                    </div>
+                </div>
+                <button onClick={createTask}>Submit</button>
+            </div>
         </div>
     )
 }
